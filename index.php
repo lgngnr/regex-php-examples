c5e564e190e52635a3d8955b09f2442d9a151913
Luigi <luiginigrelli@me.com>
2019-08-10 19:41:03 +0200

    Update README.md

--- index.php
+++ index.php
@@ -58,20 +58,48 @@
         $regex = '#<a href="(.+?)" class=".+?">.NET<\/a>#';
         echo "Get href from ancor .NET, REGEX: $regex\n";
         regExecute($regex, $html);
+
+        /**
+         * Get all php code 
+         */
+        $regex = '#<code class="language-php">([\s\S]*)<\/code>#';
+        echo "Get Get all links, REGEX: $regex\n";
+        regExecute($regex, $html, true);
+
+        /**
+         * Get all links 
+         */
+        $regex = '#(http[s{,1}]:\/\/www\..+?\..+)"#';
+        echo "Get Get all links, REGEX: $regex\n";
+        regExecute($regex, $html, true);
     }
     else
     {
         echo "Can't retrieve content";
     }
 
-    function regExecute($regex, $content){
-        if(preg_match($regex, $content, $res))
-        {
-            print_r($res);
-            echo "\n\n";
+    function regExecute($regex, $content, $all = false){
+        if($all){
+            if(preg_match_all($regex, $content, $res))
+            {
+                print_r($res);
+                echo "\n\n";
+            }
+            else
+            {
+                echo "no match\n\n";
+            }
         }
-        else
-        {
-            echo "no match\n\n";
+        else {
+            if(preg_match($regex, $content, $res))
+            {
+                print_r($res);
+                echo "\n\n";
+            }
+            else
+            {
+                echo "no match\n\n";
+            }
         }
+        
     }
